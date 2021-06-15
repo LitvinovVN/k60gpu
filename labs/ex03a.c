06320bcea10cace20b3fc4e61423242f807c77aa
LitvinovVN <LitvinovVN@rambler.ru>
2021-06-15 12:40:38 +0300

    150621

--- labs/ex03a.c
+++ labs/ex03a.c
@@ -25,9 +25,12 @@ int main(int argc, char *argv[])
  int i, np=5, mp=0;
  pid = getpid();
  printf("pid=%d -> Hello!\n",pid);
+
  for (i=0; i<np; i++)
     pids[i] = 0;
+
  i = 0; pids[i] = pid; i++;
+ 
  while (i<np){
     if (spid>0 || i==1) spid = fork();
     if (spid>0) pids[i] = spid;
@@ -41,7 +44,7 @@ int main(int argc, char *argv[])
     while (pids[mp-1] == 0)
     mp--;
  }
- 
+
  prt_proc(np,mp,pid,pids);
  return 0;
 }
