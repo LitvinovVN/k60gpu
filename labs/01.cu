fff2374901d63f723ce4d414c3845a6a28572864
LitvinovVN <LitvinovVN@rambler.ru>
2021-06-15 13:03:47 +0300

    150621

--- labs/01.cu
+++ labs/01.cu
@@ -1,131 +1,59 @@
+#include <cuda.h>
 #include <stdio.h>
 
-//
-// Nearly minimal CUDA example.
-// Compile with:
-//
-// nvcc -o example example.cu
-//
 
-#define N 1000
-
-using namespace std;
-
-//
-// A function marked __global__
-// runs on the GPU but can be called from
-// the CPU.
-//
-// This function multiplies the elements of an array
-// of ints by 2.
-//
-// The entire computation can be thought of as running
-// with one thread per array element with blockIdx.x
-// identifying the thread.
-//
-// The comparison i<N is because often it isn't convenient
-// to have an exact 1-1 correspondence between threads
-// and array elements. Not strictly necessary here.
-//
-// Note how we're mixing GPU and CPU code in the same source
-// file. An alternative way to use CUDA is to keep
-// C/C++ code separate from CUDA code and dynamically
-// compile and load the CUDA code at runtime, a little
-// like how you compile and load OpenGL shaders from
-// C/C++ code.
-//
-__global__
-void add(int *a, int *b) {
-    int i = blockIdx.x;
-    if (i<N) {
-        b[i] = 2*a[i];
-    }
+void cudasafe(int error, char* message, char* file, int line) {
+        if (error != cudaSuccess) {
+                fprintf(stderr, "CUDA Error: %s : %i. In %s line %d\n", message, error, file, line); 
+                exit(-1);
+        }
 }
 
 
-void DisplayHeader()
-{
-    const int kb = 1024;
-    const int mb = kb * kb;
-    std::wcout << "NBody.GPU" << std::endl << "=========" << std::endl << endl;
+int main(int argc, char ** argv) {
+        int deviceCount; 
 
-    std::wcout << "CUDA version:   v" << CUDART_VERSION << endl;    
-    std::wcout << "Thrust version: v" << THRUST_MAJOR_VERSION << "." << THRUST_MINOR_VERSION << endl << endl; 
+        cudasafe(cudaGetDeviceCount(&deviceCount), "GetDeviceCount", __FILE__, __LINE__); 
 
-    int devCount;
-    cudaGetDeviceCount(&devCount);
-    std::wcout << "CUDA Devices: " << std::endl << std::endl;
+        printf("Number of CUDA devices %d.\n", deviceCount); 
 
-    for(int i = 0; i < devCount; ++i)
-    {
-        cudaDeviceProp props;
-        cudaGetDeviceProperties(&props, i);
-        std::wcout << i << ": " << props.name << ": " << props.major << "." << props.minor << std::endl;
-        std::wcout << "  Global memory:   " << props.totalGlobalMem / mb << "mb" << std::endl;
-        std::wcout << "  Shared memory:   " << props.sharedMemPerBlock / kb << "kb" << std::endl;
-        std::wcout << "  Constant memory: " << props.totalConstMem / kb << "kb" << std::endl;
-        std::wcout << "  Block registers: " << props.regsPerBlock << std::endl << std::endl;
+    for (int dev = 0; dev < deviceCount; dev++) {
+        cudaDeviceProp deviceProp;
 
-        std::wcout << "  Warp size:         " << props.warpSize << std::endl;
-        std::wcout << "  Threads per block: " << props.maxThreadsPerBlock << std::endl;
-        std::wcout << "  Max block dimensions: [ " << props.maxThreadsDim[0] << ", " << props.maxThreadsDim[1]  << ", " << props.maxThreadsDim[2] << " ]" << std::endl;
-        std::wcout << "  Max grid dimensions:  [ " << props.maxGridSize[0] << ", " << props.maxGridSize[1]  << ", " << props.maxGridSize[2] << " ]" << std::endl;
-        std::wcout << std::endl;
-    }
-}
+        cudasafe(cudaGetDeviceProperties(&deviceProp, dev), "Get Device Properties", __FILE__, __LINE__);
 
+        if (dev == 0) {
+            if (deviceProp.major == 9999 && deviceProp.minor == 9999) {
+                printf("No CUDA GPU has been detected\n");
+                return -1;
+            } else if (deviceCount == 1) {
+                printf("There is 1 device supporting CUDA\n");
+            } else {
+                printf("There are %d devices supporting CUDA\n", deviceCount);
+            }
+        }
 
-int main() {
-    DisplayHeader();
+                printf("For device #%d\n", dev); 
+                printf("Device name:                %s\n", deviceProp.name); 
+                printf("Major revision number:      %d\n", deviceProp.major);
+                printf("Minor revision Number:      %d\n", deviceProp.minor); 
+                printf("Total Global Memory:        %d\n", deviceProp.totalGlobalMem);
+                printf("Total shared mem per block: %d\n", deviceProp.sharedMemPerBlock); 
+                printf("Total const mem size:       %d\n", deviceProp.totalConstMem); 
+                printf("Warp size:                  %d\n", deviceProp.warpSize); 
+                printf("Maximum block dimensions:   %d x %d x %d\n", deviceProp.maxThreadsDim[0], \
+                                                                                                          deviceProp.maxThreadsDim[1], \
+                                                                                                          deviceProp.maxThreadsDim[2]); 
 
-    //
-    // Create int arrays on the CPU.
-    // ('h' stands for "host".)
-    //
-    int ha[N], hb[N];
+                printf("Maximum grid dimensions:    %d x %d x %d\n", deviceProp.maxGridSize[0], \
+                                                                                                          deviceProp.maxGridSize[1], \
+                                                                                                          deviceProp.maxGridSize[2]); 
+                printf("Clock Rate:                 %d\n", deviceProp.clockRate); 
+                printf("Number of muliprocessors:   %d\n", deviceProp.multiProcessorCount); 
 
-    //
-    // Create corresponding int arrays on the GPU.
-    // ('d' stands for "device".)
-    //
-    int *da, *db;
-    cudaMalloc((void **)&da, N*sizeof(int));
-    cudaMalloc((void **)&db, N*sizeof(int));
-
-    //
-    // Initialise the input data on the CPU.
-    //
-    for (int i = 0; i<N; ++i) {
-        ha[i] = i;
-    }
-
-    //
-    // Copy input data to array on GPU.
-    //
-    cudaMemcpy(da, ha, N*sizeof(int), cudaMemcpyHostToDevice);
-
-    //
-    // Launch GPU code with N threads, one per
-    // array element.
-    //
-    add<<<N, 1>>>(da, db);
-
-    
-
-    //
-    // Copy output array from GPU back to CPU.
-    //
-    cudaMemcpy(hb, db, N*sizeof(int), cudaMemcpyDeviceToHost);
-
-    for (int i = 0; i<N; ++i) {
-        printf("%d\n", hb[i]);
-    }
-
-    //
-    // Free up the arrays on the GPU.
-    //
-    cudaFree(da);
-    cudaFree(db);
+   } 
 
     return 0;
+
+
 }
