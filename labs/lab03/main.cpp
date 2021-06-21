6da5e02066b28086c85bfdcba06211be37f85445
LitvinovVN <LitvinovVN@rambler.ru>
2021-06-21 15:07:41 +0300

    21.06.2021_15:07:41,18

--- labs/lab03/main.cpp
+++ labs/lab03/main.cpp
@@ -5,6 +5,19 @@ std::mutex mtx;
 static int counter = 0;
 static const int MAX_COUNTER_VAL = 100;
 
+void thread_proc(int tnum) {
+    for(;;) {
+        {
+            std::lock_guard<std::mutex> lock(mtx);
+            if(counter == MAX_COUNTER_VAL)
+                break;
+            int ctr_val = ++counter;
+            std::cout << "Thread " << tnum << ": counter = " <<
+                         ctr_val << std::endl;
+        }
+        std::this_thread::sleep_for(std::chrono::milliseconds(10));
+    }
+}
 
 int main(int argc, char* argv[]){
 	
@@ -23,7 +36,21 @@ int main(int argc, char* argv[]){
 	std::cout<<"res_cpu = "<<res_cpu<<" (rank = "<<rank<<")"<<std::endl;
 
 
+	if(rank == 0)
+	{
+		std::vector<std::thread> threads;
+		for(int i = 0; i < 10; i++) {
+			std::thread thr(thread_proc, i);
+			threads.emplace_back(std::move(thr));
+		}
 
+		// can't use const auto& here since .join() is not marked const
+		for(auto& thr : threads) {
+			thr.join();
+		}
+
+		std::cout << "Done!" << std::endl;
+	}
 
 	
 	MPI_Finalize();
