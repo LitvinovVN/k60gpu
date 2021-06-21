d8b0babfe74c994ddff6b07eb0724b269fa70768
LitvinovVN <LitvinovVN@rambler.ru>
2021-06-21 14:47:50 +0300

    210621

---
+++ labs/lab03/head.h
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <mpi.h>
+#include <mutex>
+#include <thread>
+#include <vector>
+
+#include "cpu.h"
+#include "gpu.h"
---
+++ labs/lab03/main.cpp
@@ -0,0 +1,25 @@
+#include "head.h"
+
+int main(int argc, char* argv[]){
+	
+	int rank, size;
+	int x = 25;
+	int y = 20;
+	
+	MPI_Init (&argc, &argv);
+	MPI_Comm_rank (MPI_COMM_WORLD, &rank);//номер текущего процесса
+    MPI_Comm_size (MPI_COMM_WORLD, &size);//число процессов
+	
+	int res_gpu = gpu(x, y);	
+	int res_cpu = cpu(x, y);
+	
+	std::cout<<"res_gpu = "<<res_gpu<<" (rank = "<<rank<<")"<<std::endl;
+	std::cout<<"res_cpu = "<<res_cpu<<" (rank = "<<rank<<")"<<std::endl;
+
+
+
+	
+	
+	MPI_Finalize();
+	return 0;
+}
