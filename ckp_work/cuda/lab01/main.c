e3b984cbe8c4962947c4422f6b02c63c07fafc48
LitvinovVN <LitvinovVN@rambler.ru>
2021-06-21 10:23:58 +0300

    21.06.2021_10:23:58,10

--- ckp_work/cuda/lab01/main.c
+++ ckp_work/cuda/lab01/main.c
@@ -7,7 +7,7 @@ int main (int argc, char* argv[])
     MPI_Init (&argc, &argv);
     MPI_Comm_rank (MPI_COMM_WORLD, &rank);
     MPI_Comm_size (MPI_COMM_WORLD, &size);	
-	std::cout<<"\nHello MPI!!!"<<std::endl;
+	std::cout<<"\nHello MPI!!!"<<" My rank is"<<rank<<std::endl;
     MPI_Finalize();
     return 0;
 }
