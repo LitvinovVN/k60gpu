62270d836257ffdf9b9391200c6675679dd935b1
LitvinovVN <LitvinovVN@rambler.ru>
2021-06-21 13:32:26 +0300

    21.06.2021_13:32:26,91

--- labs/lab02/main.cpp
+++ labs/lab02/main.cpp
@@ -13,8 +13,8 @@ int main(int argc, char* argv[]){
 	int res_gpu = gpu(x, y);	
 	int res_cpu = cpu(x, y);
 	
-	std::cout<<"res_gpu = "<<res_gpu<<" (rank = )"<<rank<<std::endl;
-	std::cout<<"res_cpu = "<<res_cpu<<" (rank = )"<<rank<<std::endl;
+	std::cout<<"res_gpu = "<<res_gpu<<" (rank = "<<rank<<")"<<std::endl;
+	std::cout<<"res_cpu = "<<res_cpu<<" (rank = "<<rank<<")"<<std::endl;
 	
 	MPI_Finalize();
 	return 0;
