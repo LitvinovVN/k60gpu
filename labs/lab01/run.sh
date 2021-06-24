e737ba26e309bb70fc7865c6549f2d211f30d083
LitvinovVN <LitvinovVN@rambler.ru>
2021-06-24 12:03:49 +0300

    24.06.2021_12:03:49,47

--- labs/lab01/run.sh
+++ labs/lab01/run.sh
@@ -8,4 +8,10 @@ echo '-------------'
 mpicxx -o myapp main.o
 echo '-------------'
 
+echo '------- Starting myapp in 1 node with 1 cpu per node with maxtime 60 minutes ------'
+echo 'mpirun -np 1 -ppn 1 -maxtime 60 ./myapp'
 mpirun -np 1 -ppn 1 -maxtime 60 ./myapp
+
+echo '------- Status of executed task: mqtest myapp.1 --------'
+mqtest myapp.1
+echo '------- End of run.sh ------'
