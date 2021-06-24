08b141f71fae091198fb3a692bee7b5bc702cdd4
LitvinovVN <LitvinovVN@rambler.ru>
2021-06-24 14:38:51 +0300

    24.06.2021_14:38:51,29

--- labs/lab01/main.c
+++ labs/lab01/main.c
@@ -2,15 +2,18 @@
 #include <iostream> 
 #include <unistd.h>
 
+using namespace std;
+
 int main (int argc, char* argv[])
-{
+{    
+
     int rank, size;
     MPI_Init (&argc, &argv);
     MPI_Comm_rank (MPI_COMM_WORLD, &rank);
     MPI_Comm_size (MPI_COMM_WORLD, &size);
     
     if(rank==0)
-        std::cout << "First MPI program." << " Size is " << size << std::endl;
+        cout << "First MPI program." << " Size is " << size << endl;
 
     double t = MPI_Wtime();
 
