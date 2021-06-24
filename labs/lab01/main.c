c1fd6709ec9c89f4303ac32ce3ff3241612153f5
LitvinovVN <LitvinovVN@rambler.ru>
2021-06-24 13:30:05 +0300

    24.06.2021_13:30:05,31

--- labs/lab01/main.c
+++ labs/lab01/main.c
@@ -8,17 +8,15 @@ int main (int argc, char* argv[])
     MPI_Init (&argc, &argv);
     MPI_Comm_rank (MPI_COMM_WORLD, &rank);
     MPI_Comm_size (MPI_COMM_WORLD, &size);
-
-    double t = MPI_Wtime();
-    printf("Time: %lf sec\n",t);
-
+    
     if(rank==0)
-        std::cout<<"Hello MPI!!!"<<" Size is "<<size<<std::endl;
+        std::cout << "Hello MPI!!!" << " Size is " << size << std::endl;
 
     char hostname[50];    
     gethostname(hostname, 50);    
+    double t = MPI_Wtime();
 
-	std::cout << " My rank is " << rank << "; hostname: " << hostname << std::endl;
+	std::cout << " My rank is " << rank << "; hostname: " << hostname << "; Time: " << t << std::endl;
     MPI_Finalize();
     return 0;
 }
