1f9fa276882e353b325e40e98f0d609d08e3ea66
LitvinovVN <LitvinovVN@rambler.ru>
2021-06-24 13:02:24 +0300

    24.06.2021_13:02:24,49

--- labs/lab01/clear.sh
+++ labs/lab01/clear.sh
@@ -2,8 +2,12 @@ clear
 
 echo '------ Stopping task myapp.1: mcancel myapp.1 -------'
 mcancel myapp.1
+echo '------ Stopping task myapp.2: mcancel myapp.2 -------'
+mcancel myapp.2
+echo '------ Stopping task myapp.3: mcancel myapp.3 -------'
+mcancel myapp.3
+
 echo '------ Removing temporary files -------'
-rm -r myapp.1
 rm -r myapp.*
 rm *.o myapp
 echo '------ Temporary files removed ------'
