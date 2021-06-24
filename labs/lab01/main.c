2471e5290444d3006d7611e0ea6e6b09b8c91ec2
LitvinovVN <LitvinovVN@rambler.ru>
2021-06-24 13:25:27 +0300

    24.06.2021_13:25:27,27

--- labs/lab01/main.c
+++ labs/lab01/main.c
@@ -9,6 +9,9 @@ int main (int argc, char* argv[])
     MPI_Comm_rank (MPI_COMM_WORLD, &rank);
     MPI_Comm_size (MPI_COMM_WORLD, &size);
 
+    double t = MPI_Wtime();
+    printf("Time: %lf sec\n",t);
+
     if(rank==0)
         std::cout<<"Hello MPI!!!"<<" Size is "<<size<<std::endl;
 
