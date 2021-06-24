cc179773ee032553d4f007ff00b8a4eeb2d4a5cb
LitvinovVN <LitvinovVN@rambler.ru>
2021-06-24 10:36:18 +0300

    24.06.2021_10:36:18,83

--- labs/lab01/run.sh
+++ labs/lab01/run.sh
@@ -1,10 +1,7 @@
+git pull
+
 cat description.md
 
-git pull
-bash clear.sh
-
-echo '------ mpicxx -show -------'
-mpicxx -show
 echo '------- Compiling main.c into main.o ------'
 mpicxx -O3 -c main.c
 echo '-------------'
@@ -12,11 +9,3 @@ mpicxx -o myapp main.o
 echo '-------------'
 
 mpirun -np 8 ./myapp
-
-echo '----- sleep 5 seconds -------'
-sleep 5
-
-echo '-------------------'
-echo '-----RESULTS-------'
-echo '-------------------'
-cat ./myapp.1/output
