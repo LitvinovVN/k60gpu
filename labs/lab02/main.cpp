dc04cb90e70b6d888b17f9f90e44367afbd72b87
LitvinovVN <LitvinovVN@rambler.ru>
2021-06-25 10:49:11 +0300

    25.06.2021_10:49:11,82

--- labs/lab02/main.cpp
+++ labs/lab02/main.cpp
@@ -5,7 +5,9 @@
 
 using namespace std;
 
-
+void thread_proc(int tnum, char* hostname, int rank) {
+    fprintf(stderr, "Time: %lf. Hostname: %s. MPI rank: %d. Process ID: %d. Thread index: %d \n", MPI_Wtime(), hostname, rank, getpid(), tnum);
+}
 
 int main (int argc, char* argv[])
 {
@@ -23,7 +25,16 @@ int main (int argc, char* argv[])
     pid_t pid = getpid();
     int nHardwareThreads = std::thread::hardware_concurrency();
     fprintf(stderr, "Time: %lf. Hostname: %s. MPI rank: %d. Process ID: %d. Hardware threads: %d \n", t, hostname, rank, pid, nHardwareThreads);
-        	
+
+    std::vector<std::thread> threads;
+	for(int i = 0; i < nHardwareThreads-1; i++) {
+		std::thread thr(thread_proc, i, hostname, rank);
+		threads.emplace_back(std::move(thr));
+	}
+	
+	for(auto& thr : threads) {
+		thr.join();
+	}    	
 
     MPI_Finalize();
     return 0;
