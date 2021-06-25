b8364ccb078eeb4790d08ae348dafe3fb679770a
LitvinovVN <LitvinovVN@rambler.ru>
2021-06-25 16:28:54 +0300

    25.06.2021_16:28:54,03

--- labs/lab03/main.cpp
+++ labs/lab03/main.cpp
@@ -3,6 +3,7 @@
 #include <unistd.h>
 #include <thread>
 #include <vector>
+std::mutex g_lock;
 
 #include "cpu.h"
 #include "gpu.h"
@@ -10,9 +11,11 @@
 using namespace std;
 
 void thread_proc(int tnum, char* hostname, int rank) {
+    g_lock.lock();
     double start = MPI_Wtime();
     std::this_thread::sleep_for(std::chrono::seconds(1));
     fprintf(stderr, "Time: %lf s. Hostname: %s. MPI rank: %d. Process ID: %d. Thread index: %d \n", MPI_Wtime() - start, hostname, rank, getpid(), tnum);
+    g_lock.unlock();
 }
 
 int main (int argc, char* argv[])
