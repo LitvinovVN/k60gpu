d0c9a315644e58f1bd8143af7859709d23063c4c
LitvinovVN <LitvinovVN@rambler.ru>
2021-06-25 09:35:59 +0300

    25.06.2021_ 9:35:59,85

--- labs/lab02/main.cpp
+++ labs/lab02/main.cpp
@@ -1,122 +1,12 @@
 #include <mpi.h>
 #include <iostream> 
 #include <unistd.h>
-#include <atomic>
-#include <vector>
-#include <thread>
+//#include <atomic>
+//#include <vector>
+//#include <thread>
 
 using namespace std;
 
-template<typename T>
-class threadsafe_queue 
-{
-private:
-    std::unique_ptr<node> try_pop_head()
-    {
-        std::lock_guard<std::mutex> head_lock(head_mutex); 
-        if(head.get()==get_tail())
-        {
-            return std::unique_ptr<node>();
-        }
-        return pop_head();
-    }
-    std::unique_ptr<node> try_pop_head(T& value)
-    {
-        std::lock_guard<std::mutex> head_lock(head_mutex); 
-        if(head.get()==get_tail())
-        {
-            return std::unique_ptr<node>();
-        }
-        value=std::move(*head->data); 
-        return pop_head();
-    }
-public:
-    std::shared_ptr<T> try_pop()
-    {
-        std::unique_ptr<node> old_head=try_pop_head(); 
-        return old_head?old_head->data:std::shared_ptr<T>();
-    }
-    bool try_pop(T& value)
-    {
-        std::unique_ptr<node> const old_head=try_pop_head(value); 
-        return old_head;
-    }
-    bool empty()
-    {
-        std::lock_guard<std::mutex> head_lock(head_mutex); 
-        return (head.get()==get_tail());
-    }
-};
-
-class join_threads
-{
-    std::vector<std::thread>& threads; 
-public:
-    explicit join_threads(std::vector<std::thread>& threads_): 
-        threads(threads_)
-    {}
-    ~join_threads()
-    {
-        for(unsigned long i=0;i<threads.size();++i)
-        {
-            if (threads[i].joinable()) 
-                threads[i].join();
-        }
-    }
-};
-
-
-class thread_pool 
-{
-    std::atomic_bool done;
-    threadsafe_queue<std::function<void()>> work_queue;
-    std::vector<std::thread> threads;
-    join_threads joiner;
-    void worker_thread()
-    {
-        while(!done)
-        {
-            std::function<void()> task; 
-            if(work_queue.try_pop(task))
-            {
-                task();
-            }
-            else
-            {
-                std::this_thread::yield();
-            }
-        }
-    }
-public:
-    thread_pool():
-    done(false), joiner(threads)
-    {
-        unsigned const thread_count = std::thread::hardware_concurrency();
-        try 
-        {
-            for(unsigned i=0; i<thread_count; ++i)
-            {
-                threads.push_back(std::thread(&thread_pool::worker_thread, this));
-            }
-        }
-        catch (...)
-        {
-            done = true;
-            throw;
-        }
-    }
-
-    ~thread_pool()
-    {
-        done = true;
-    }
-
-    template<typename FunctionType> 
-    void submit(FunctionType f)
-    {
-        work_queue.push(std::function< void()> (f));
-    }
-};
 
 
 int main (int argc, char* argv[])
@@ -130,15 +20,13 @@ int main (int argc, char* argv[])
         cout << "First MPI program." << " Size is " << size << endl;
 
     double t = MPI_Wtime();
-
     char hostname[50];    
     gethostname(hostname, 50);
-
     pid_t pid = getpid();
+    int nHardwareThreads = std::thread::hardware_concurrency();
+    fprintf(stderr, "Time: %lf. Hostname: %s. MPI rank: %d. Process ID: %d. Hardware threads: %d \n", t, hostname, rank, pid, nHardwareThreads);
+        	
 
-        
-    fprintf(stderr, "Time: %lf. Hostname: %s. MPI rank: %d. Process ID: %d. \n", t, hostname, rank, pid);
-    
     MPI_Finalize();
     return 0;
 }
