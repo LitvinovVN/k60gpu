e4f481e6da9ea32b3f50779134f501289da7e9e4
LitvinovVN <LitvinovVN@rambler.ru>
2021-06-30 16:34:42 +0300

    30.06.2021_16:34:42,76

--- labs/lab03/main.cpp
+++ labs/lab03/main.cpp
@@ -51,9 +51,15 @@ int main (int argc, char* argv[])
             printf("The threading support level corresponds to that demanded.\n");
         }
 
-        time_t epoch = 0;
-        printf("%jd seconds since the epoch began\n", (intmax_t)epoch);
-        printf("%s", asctime(gmtime(&epoch)));
+        auto start = std::chrono::system_clock::now();
+        // Some computation here
+        auto end = std::chrono::system_clock::now();
+
+        std::chrono::duration<double> elapsed_seconds = end-start;
+        std::time_t end_time = std::chrono::system_clock::to_time_t(end);
+
+        std::cout << "finished computation at " << std::ctime(&end_time)
+                << "elapsed time: " << elapsed_seconds.count() << "s\n";
     }
 
     double t = MPI_Wtime();
