7fb15bf5057a45f4c65ce01555b77fe49e1d055b
LitvinovVN <LitvinovVN@rambler.ru>
2021-07-01 07:51:32 +0300

    01.07.2021_ 7:51:32,71

--- labs/lab03/run.sh
+++ labs/lab03/run.sh
@@ -12,7 +12,7 @@ echo '-------------'
 echo '------- Compiling main.cpp into main.o: mpicxx -O3 -std=c++11 -c main.cpp ------'
 mpicxx -O3 -std=c++11 -c main.cpp
 echo '------ Creating executable file myapp -------'
-mpicxx -L/usr/local/cuda/lib64 -lcudart -lm  -o myapp main.o cpu.o gpu.o
+mpicxx -L/usr/local/cuda/lib64 -lcudart -lm  -o myapp3 main.o cpu.o gpu.o
 echo '-------------'
 
 echo '------- Starting myapp in 1 node with 4 cpu per node with maxtime by default (5 minutes) ------'
@@ -20,7 +20,7 @@ echo '------- mpirun -np 4 ./myapp --------'
 #mpirun -np 4 ./myapp
 
 echo '------- mpirun -np 1 -ppn 1 -maxtime 2 ./myapp --------'
-mpirun -np 1 -ppn 1 -maxtime 2 ./myapp
+mpirun -np 1 -ppn 1 -maxtime 2 ./myapp3
 
 echo '------- mpirun -np 2 -ppn 1 -maxtime 2 ./myapp --------'
 #mpirun -np 2 -ppn 1 -maxtime 2 ./myapp
@@ -32,7 +32,7 @@ echo '------- Task list: pult t ---------'
 pult t
 
 echo '------- Status of executed task: mqtest myapp.1 --------'
-mqtest myapp.1
-mqtest myapp.2
-mqtest myapp.3
+mqtest myapp3.1
+#mqtest myapp.2
+#mqtest myapp.3
 echo '------- End of run.sh ------'
