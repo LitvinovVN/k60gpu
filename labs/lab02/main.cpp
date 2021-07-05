d3fb9d2f2bb74e7ca5d395ba3456ff7a8b3b58c6
LitvinovVN <LitvinovVN@rambler.ru>
2021-07-05 11:10:06 +0300

    05.07.2021_11:10:06,21

--- labs/lab02/main.cpp
+++ labs/lab02/main.cpp
@@ -21,7 +21,7 @@ void thread_proc(int tnum, char* hostname, int rank) {
 }
 
 
-void mpi_init(int argc, char* argv[], int& provided, int& rank, int& size){
+void mpi_init(int argc, char* argv[], int provided, int& rank, int& size){
     MPI_Init_thread(&argc, &argv, MPI_THREAD_FUNNELED, &provided);// MPI_THREAD_MULTIPLE
     MPI_Comm_rank (MPI_COMM_WORLD, &rank);
     MPI_Comm_size (MPI_COMM_WORLD, &size);
@@ -47,7 +47,7 @@ void mpi_init(int argc, char* argv[], int& provided, int& rank, int& size){
 int main (int argc, char* argv[])
 {    
     int rank, size, provided;
-    mpi_init(&argc, &argv, MPI_THREAD_FUNNELED, &provided, &rank, &size);
+    mpi_init(argc, argv, MPI_THREAD_FUNNELED, &provided, &rank, &size);
 
     
     
