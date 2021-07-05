5f23ba0e195f7c89e173c4e35a60528a108cc4ba
LitvinovVN <LitvinovVN@rambler.ru>
2021-07-05 13:47:58 +0300

    05.07.2021_13:47:58,31

--- labs/lab02/main.cpp
+++ labs/lab02/main.cpp
@@ -26,36 +26,44 @@ void mpi_init(int argc, char* argv[], int mpi_thread_type, int &provided, int &r
     MPI_Comm_rank (MPI_COMM_WORLD, &rank);
     MPI_Comm_size (MPI_COMM_WORLD, &size);
 
-    if(rank!=0) return;
-    
-    printf("argc: %d\n", argc);
-    printf("&argc: %p\n", (void*)&argc);
-    
-    for(int i=0;i<argc;i++)
-    {
-        printf("argv[%d]: %s\n", i, argv[i]);
-    }
+    double t = MPI_Wtime();
+    char hostname[50];    
+    gethostname(hostname, 50);
+    pid_t pid = getpid();
+    int nHardwareThreads = std::thread::hardware_concurrency();
+    fprintf(stderr, "Time: %lf. Hostname: %s. MPI rank: %d. Process ID: %d. Hardware threads: %d \n", t, hostname, rank, pid, nHardwareThreads);
 
-    printf("mpi_thread_type: %d\n", mpi_thread_type);
+    if(rank==0) {
+        printf("argc: %d\n", argc);
+        printf("&argc: %p\n", (void*)&argc);
+        
+        for(int i=0;i<argc;i++)
+        {
+            printf("argv[%d]: %s\n", i, argv[i]);
+        }
 
-    printf("provided: %d\n",   provided);
-    printf("&provided: %p\n", &provided);   
+        printf("mpi_thread_type: %d\n", mpi_thread_type);
+
+        printf("provided: %d\n",   provided);
+        printf("&provided: %p\n", &provided);
+
+        printf("size: %d\n",   size);
+        printf("&size: %p\n", &size);
+
+        if(provided < MPI_THREAD_FUNNELED)// MPI_THREAD_MULTIPLE
+        {
+            printf("The threading support level is lesser than that demanded.\n");
+            MPI_Abort(MPI_COMM_WORLD, EXIT_FAILURE);
+        }
+        else
+        {
+            printf("The threading support level: MPI_THREAD_FUNNELED\n");
+        }
+    }        
 
     printf("rank: %d\n",   rank);
-    printf("&rank: %p\n", &rank);
-
-    printf("size: %d\n",   size);
-    printf("&size: %p\n", &size);
+    printf("&rank: %p\n", &rank);    
     
-    if(provided < MPI_THREAD_FUNNELED)// MPI_THREAD_MULTIPLE
-    {
-        printf("The threading support level is lesser than that demanded.\n");
-        MPI_Abort(MPI_COMM_WORLD, EXIT_FAILURE);
-    }
-    else
-    {
-        printf("The threading support level: MPI_THREAD_FUNNELED\n");
-    }
 
     printf("hardware_concurrency(): %d\n", std::thread::hardware_concurrency());
 }
@@ -63,21 +71,12 @@ void mpi_init(int argc, char* argv[], int mpi_thread_type, int &provided, int &r
 
 int main (int argc, char* argv[])
 {    
-    int rank, size, provided;
-    
-    //MPI_Init_thread(&argc, &argv, MPI_THREAD_FUNNELED, &provided);// MPI_THREAD_MULTIPLE
-    
-    
+    int rank, size, provided;    
     mpi_init(argc, argv, MPI_THREAD_FUNNELED, provided, rank, size);     
 
+      
     
-    
-    double t = MPI_Wtime();
-    char hostname[50];    
-    gethostname(hostname, 50);
-    pid_t pid = getpid();
-    int nHardwareThreads = std::thread::hardware_concurrency();
-    fprintf(stderr, "Time: %lf. Hostname: %s. MPI rank: %d. Process ID: %d. Hardware threads: %d \n", t, hostname, rank, pid, nHardwareThreads);
+
 
     std::vector<std::thread> threads;
 	for(int i = 0; i < nHardwareThreads-1; i++) {
