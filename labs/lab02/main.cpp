b57c050e139daa3eff856cc5559c6b091edd39ef
LitvinovVN <LitvinovVN@rambler.ru>
2021-07-05 12:09:31 +0300

    05.07.2021_12:09:31,21

--- labs/lab02/main.cpp
+++ labs/lab02/main.cpp
@@ -39,11 +39,11 @@ int main (int argc, char* argv[])
 {    
     int rank, size, provided;
     
-    MPI_Init_thread(&argc, &argv, MPI_THREAD_FUNNELED, provided);// MPI_THREAD_MULTIPLE
+    MPI_Init_thread(&argc, &argv, MPI_THREAD_FUNNELED, &provided);// MPI_THREAD_MULTIPLE
     MPI_Comm_rank (MPI_COMM_WORLD, &rank);
     MPI_Comm_size (MPI_COMM_WORLD, &size);
     
-    mpi_init(argc, argv, MPI_THREAD_FUNNELED, &provided/*, &rank, &size*/);
+    mpi_init(argc, argv, MPI_THREAD_FUNNELED, provided/*, &rank, &size*/);
 
     if(rank==0){
         cout << "MPI size is " << size << endl;
