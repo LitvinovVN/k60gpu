5f054da6ffc8fde6984186cfed91db77bd0d0168
LitvinovVN <LitvinovVN@rambler.ru>
2021-07-06 15:51:58 +0300

    06.07.2021_15:51:58,15

--- labs/lab02/main.cpp
+++ labs/lab02/main.cpp
@@ -11,13 +11,6 @@
 
 using namespace std;
 
-void printTime(std::string timePointDescr){
-    auto now = std::chrono::system_clock::now();
-    time_t tt = std::chrono::system_clock::to_time_t(now);  
-
-    std::cout << std::put_time(std::localtime(&tt), "%X") << ": " << timePointDescr << std::endl;
-    std::cerr << "errors";
-}
 
 void thread_proc(int tnum, int rank) {
     fprintf(stderr, "Thread %d started at node %d... \n", tnum, rank);
@@ -33,8 +26,7 @@ void thread_proc(int tnum, int rank) {
     fprintf(stderr, "Time: %lf. MPI rank: %d. Process ID: %d. Thread index: %d. pauseTime = %d \n", MPI_Wtime(), rank, getpid(), tnum, pauseTime);    
 }
 
-void testThreads(int rank){
-    printTime("testThreads started.\n");
+void testThreads(int rank){    
     std::vector<std::thread> threads;
 	for(int i = 0; i < std::thread::hardware_concurrency()-1; i++) {
 		std::thread thr(thread_proc, i, rank);
@@ -43,8 +35,7 @@ void testThreads(int rank){
 	
 	for(auto& thr : threads) {
 		thr.join();
-	}
-    printTime("testThreads ended.\n");
+	}    
 }
 
 int main (int argc, char* argv[])
@@ -52,8 +43,12 @@ int main (int argc, char* argv[])
     int rank, size, provided;    
     mpi_init(argc, argv, MPI_THREAD_FUNNELED, provided, rank, size);     
     
-    testThreads(rank);   
-    
+    double t1 = MPI_Wtime();
+    testThreads(rank); 
+    double t2 = MPI_Wtime();  
+    double t = t2-t1;
+    printf("Rank %d: Time of testThreads: %lf sec\n",rank, t);
+
     MPI_Finalize();
     return 0;
 }
