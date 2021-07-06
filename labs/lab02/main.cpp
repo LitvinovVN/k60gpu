31f7710b7bd7175313ab6f188646d53c3eb92e3d
LitvinovVN <LitvinovVN@rambler.ru>
2021-07-06 15:30:43 +0300

    06.07.2021_15:30:43,81

--- labs/lab02/main.cpp
+++ labs/lab02/main.cpp
@@ -9,22 +9,6 @@
 
 using namespace std;
 
-struct Stopwatch {
-    Stopwatch()
-     : _start{ std::chrono::high_resolution_clock::now() }
-    { }
-
-    ~Stopwatch() {
-        _elapsed = std::chrono::high_resolution_clock::now() - _start;
-        auto time = _elapsed.count();
-        printf("Took %g ns.\n", time);
-    }
-
-private:
-    std::chrono::nanoseconds _elapsed;
-    const std::chrono::time_point<std::chrono::high_resolution_clock> _start;
-};
-
 
 void thread_proc(int tnum, int rank) {
     fprintf(stderr, "Thread %d started at node %d... \n", tnum, rank);
@@ -57,22 +41,7 @@ int main (int argc, char* argv[])
     int rank, size, provided;    
     mpi_init(argc, argv, MPI_THREAD_FUNNELED, provided, rank, size);     
     
-    testThreads(rank);
-
-
-    const size_t n = 1000000;
-    //std::chrono::nanoseconds elapsed;
-    {
-        printf("Stopwatch!\n");
-        Stopwatch stopwatch{/*elapsed*/};
-        volatile double result{1.23e45};
-        for(double i=1; i<n; i++)
-        {
-            result /= i;
-        }
-    }
-    //auto time_per_division = elapsed.count() / double{n};
-    //printf("Took %g ns per division.\n", time_per_division);
+    testThreads(rank);   
     
     MPI_Finalize();
     return 0;
