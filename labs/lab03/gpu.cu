72f981672f1b3c3742ee1979c4ca55da5a6e0712
LitvinovVN <LitvinovVN@rambler.ru>
2021-07-08 12:47:14 +0300

    08.07.2021_12:47:14,06

--- labs/lab03/gpu.cu
+++ labs/lab03/gpu.cu
@@ -32,6 +32,11 @@ void printGpuParameters(std::string prefixDescr) {
 		ss << "Max threads per block  : " << devProp.maxThreadsPerBlock << std::endl;
 		ss << "Total constant memory  : " << devProp.totalConstMem << " bytes" << std::endl;
 		ss << "Clock rate             : " << devProp.clockRate << " Hz" << std::endl;
+		ss << "Texture Alignment      : " << devProp.textureAlignment << std::endl;
+		ss << "Device Overlap         : " << devProp.deviceOverlap << std::endl;
+		ss << "Multiprocessor Count   : " << devProp.multiProcessorCount << " Hz" << std::endl;
+		ss << "Max Threads Dim        : " << devProp.maxThreadsDim[0] << " " << devProp.maxThreadsDim[1] << " " << devProp.maxThreadsDim[2] << std::endl;
+		ss << "Max Grid Num           : " << devProp.maxGridSize[0] << " " << devProp.maxGridSize[1] << " " << devProp.maxGridSize[2] << std::endl;
 	}
 
     std::cout << ss.str();
