92cc7f7207068f5a6eaaea0871e66022a0cea6ba
LitvinovVN <LitvinovVN@rambler.ru>
2021-07-12 08:11:22 +0300

    12.07.2021_ 8:11:22,28

--- labs/lab04/gpu.cu
+++ labs/lab04/gpu.cu
@@ -66,14 +66,19 @@ int gpu(int x, int y){
 
 
 //////////////////////////////////////////////////////////////////////
+#define     N    (33*1024*1024)
+	const int threadsPerBlock = 256;
+	const int blocksPerGrid =
+            imin( 32, (N/2+threadsPerBlock-1) / threadsPerBlock );
+
 static void HandleError( cudaError_t err,
 	const char *file,
 	int line ) {
-if (err != cudaSuccess) {
-printf( "%s in %s at line %d\n", cudaGetErrorString( err ),
-file, line );
-exit( EXIT_FAILURE );
-}
+		if (err != cudaSuccess) {
+		printf( "%s in %s at line %d\n", cudaGetErrorString( err ),
+		file, line );
+		exit( EXIT_FAILURE );
+	}
 }
 #define HANDLE_ERROR( err ) (HandleError( err, __FILE__, __LINE__ ))
 
@@ -82,6 +87,94 @@ exit( EXIT_FAILURE );
 	   printf( "Host memory failed in %s at line %d\n", \
 			   __FILE__, __LINE__ ); \
 	   exit( EXIT_FAILURE );}}
+
+
+
+	   #if _WIN32
+	   //Windows threads.
+	   #include <windows.h>
+   
+	   typedef HANDLE CUTThread;
+	   typedef unsigned (WINAPI *CUT_THREADROUTINE)(void *);
+   
+	   #define CUT_THREADPROC unsigned WINAPI
+	   #define  CUT_THREADEND return 0
+   
+   #else
+	   //POSIX threads.
+	   #include <pthread.h>
+   
+	   typedef pthread_t CUTThread;
+	   typedef void *(*CUT_THREADROUTINE)(void *);
+   
+	   #define CUT_THREADPROC void
+	   #define  CUT_THREADEND
+   #endif
+   
+   //Create thread.
+   CUTThread start_thread( CUT_THREADROUTINE, void *data );
+   
+   //Wait for thread to finish.
+   void end_thread( CUTThread thread );
+   
+   //Destroy thread.
+   void destroy_thread( CUTThread thread );
+   
+   //Wait for multiple threads.
+   void wait_for_threads( const CUTThread *threads, int num );
+   
+   #if _WIN32
+	   //Create thread
+	   CUTThread start_thread(CUT_THREADROUTINE func, void *data){
+		   return CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)func, data, 0, NULL);
+	   }
+   
+	   //Wait for thread to finish
+	   void end_thread(CUTThread thread){
+		   WaitForSingleObject(thread, INFINITE);
+		   CloseHandle(thread);
+	   }
+   
+	   //Destroy thread
+	   void destroy_thread( CUTThread thread ){
+		   TerminateThread(thread, 0);
+		   CloseHandle(thread);
+	   }
+   
+	   //Wait for multiple threads
+	   void wait_for_threads(const CUTThread * threads, int num){
+		   WaitForMultipleObjects(num, threads, true, INFINITE);
+   
+		   for(int i = 0; i < num; i++)
+			   CloseHandle(threads[i]);
+	   }
+   
+   #else
+	   //Create thread
+	   CUTThread start_thread(CUT_THREADROUTINE func, void * data){
+		   pthread_t thread;
+		   pthread_create(&thread, NULL, func, data);
+		   return thread;
+	   }
+   
+	   //Wait for thread to finish
+	   void end_thread(CUTThread thread){
+		   pthread_join(thread, NULL);
+	   }
+   
+	   //Destroy thread
+	   void destroy_thread( CUTThread thread ){
+		   pthread_cancel(thread);
+	   }
+   
+	   //Wait for multiple threads
+	   void wait_for_threads(const CUTThread * threads, int num){
+		   for(int i = 0; i < num; i++)
+			   end_thread( threads[i] );
+	   }
+   
+   #endif
+
 
 
 __global__ void dot( int size, float *a, float *b, float *c ) {
@@ -179,102 +272,8 @@ void* routine( void *pvoidData ) {
 
 
 
-
-#if _WIN32
-    //Windows threads.
-    #include <windows.h>
-
-    typedef HANDLE CUTThread;
-    typedef unsigned (WINAPI *CUT_THREADROUTINE)(void *);
-
-    #define CUT_THREADPROC unsigned WINAPI
-    #define  CUT_THREADEND return 0
-
-#else
-    //POSIX threads.
-    #include <pthread.h>
-
-    typedef pthread_t CUTThread;
-    typedef void *(*CUT_THREADROUTINE)(void *);
-
-    #define CUT_THREADPROC void
-    #define  CUT_THREADEND
-#endif
-
-//Create thread.
-CUTThread start_thread( CUT_THREADROUTINE, void *data );
-
-//Wait for thread to finish.
-void end_thread( CUTThread thread );
-
-//Destroy thread.
-void destroy_thread( CUTThread thread );
-
-//Wait for multiple threads.
-void wait_for_threads( const CUTThread *threads, int num );
-
-#if _WIN32
-    //Create thread
-    CUTThread start_thread(CUT_THREADROUTINE func, void *data){
-        return CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)func, data, 0, NULL);
-    }
-
-    //Wait for thread to finish
-    void end_thread(CUTThread thread){
-        WaitForSingleObject(thread, INFINITE);
-        CloseHandle(thread);
-    }
-
-    //Destroy thread
-    void destroy_thread( CUTThread thread ){
-        TerminateThread(thread, 0);
-        CloseHandle(thread);
-    }
-
-    //Wait for multiple threads
-    void wait_for_threads(const CUTThread * threads, int num){
-        WaitForMultipleObjects(num, threads, true, INFINITE);
-
-        for(int i = 0; i < num; i++)
-            CloseHandle(threads[i]);
-    }
-
-#else
-    //Create thread
-    CUTThread start_thread(CUT_THREADROUTINE func, void * data){
-        pthread_t thread;
-        pthread_create(&thread, NULL, func, data);
-        return thread;
-    }
-
-    //Wait for thread to finish
-    void end_thread(CUTThread thread){
-        pthread_join(thread, NULL);
-    }
-
-    //Destroy thread
-    void destroy_thread( CUTThread thread ){
-        pthread_cancel(thread);
-    }
-
-    //Wait for multiple threads
-    void wait_for_threads(const CUTThread * threads, int num){
-        for(int i = 0; i < num; i++)
-            end_thread( threads[i] );
-    }
-
-#endif
-
-
-
 extern "C" void multiGpuTest(){
 	std::cerr << "multiGpuTest()" << std::endl;
-
-	#define     N    (33*1024*1024)
-	const int threadsPerBlock = 256;
-	const int blocksPerGrid =
-            imin( 32, (N/2+threadsPerBlock-1) / threadsPerBlock );
-
 
 	int deviceCount;
     HANDLE_ERROR( cudaGetDeviceCount( &deviceCount ) );
