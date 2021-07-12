60bfbad2e0a82823520d318d82c811a61573b9e5
LitvinovVN <LitvinovVN@rambler.ru>
2021-07-12 15:46:09 +0300

    12.07.2021_15:46:09,14

--- labs/lab05_sum_2_arr_MPI+threads+cuda/main.cpp
+++ labs/lab05_sum_2_arr_MPI+threads+cuda/main.cpp
@@ -7,17 +7,22 @@
 
 using namespace std;
 
+void testSum2Arrays(rank, size)
+{
+    cout << "testSum2Arrays" << std::endl;
+}
+
 int main (int argc, char* argv[])
 {    
     int rank, size, provided;    
     mpi_init(argc, argv, MPI_THREAD_FUNNELED, provided, rank, size);     
     MPI_Barrier( MPI_COMM_WORLD );
 
-    //double t1 = MPI_Wtime();
-    //testThreads(rank); 
-    //double t2 = MPI_Wtime();  
-    //double t = t2-t1;
-    //printf("Rank %d: Time of testThreads: %lf sec\n",rank, t);
+    double t1 = MPI_Wtime();
+    testSum2Arrays(rank, size); 
+    double t2 = MPI_Wtime();  
+    double t = t2-t1;
+    printf("Rank %d: Time of testThreads: %lf sec\n",rank, t);
 
     //printGpuParameters("Node " + std::to_string(rank));
 
