589d7975119780186e914e62fd29730b6e7a7f75
LitvinovVN <LitvinovVN@rambler.ru>
2021-07-13 16:08:53 +0300

    13.07.2021_16:08:53,21

--- labs/lab05_sum_2_arr_MPI+threads+cuda/run.sh
+++ labs/lab05_sum_2_arr_MPI+threads+cuda/run.sh
@@ -5,18 +5,15 @@ echo ''
 echo '------- Compiling main.c into main.o: mpicxx -O3 -c main.cpp ------'
 mpicxx -O3 -std=c++11 -c main.cpp
 mpicxx -O3 -std=c++11 -c utils.cpp
-echo '------- Compiling gpu.cu into gpu.o ------'
+echo '------- Compiling sum.cu into sum.o ------'
 nvcc -arch sm_70 --ptxas-options=-v  -std=c++11 -c sum.cu
-echo '-------- mpicxx -o myapp4 main.o utils.o cpuThreads.o gpu.o-----'
+echo '-------- mpicxx -L/usr/local/cuda/lib64 -lcudart -lm -o myapp5 main.o utils.o sum.o -----'
 mpicxx -L/usr/local/cuda/lib64 -lcudart -lm -o myapp5 main.o utils.o sum.o
 echo '-------------'
 
-#echo '------- Starting myapp5 in 1 node with 1 cpu per node with maxtime by 2 minutes ------'
+echo '------- Starting myapp5 in 1 node with 1 cpu per node with maxtime by 2 minutes ------'
 echo '------- mpirun -np 1 -ppn 1 -maxtime 2 ./myapp5 --------'
 mpirun -np 1 -ppn 1 -maxtime 2 ./myapp5
-
-#echo '------- mpirun -np 2 -ppn 1 -maxtime 2 ./myapp5 --------'
-#mpirun -np 2 -ppn 1 -maxtime 2 ./myapp5
 
 echo '------- Task list: mps ---------'
 mps
@@ -26,6 +23,4 @@ pult t
 
 echo '------- Status of executed task: mqtest myapp5.1 --------'
 mqtest myapp5.1
-#mqtest myapp5.2
-#mqtest myapp5.3
 echo '------- End of run.sh ------'
