c04ac69862596c51db65ccdf342bc1fbb844f317
LitvinovVN <LitvinovVN@rambler.ru>
2021-07-13 16:05:56 +0300

    13.07.2021_16:05:56,37

--- labs/lab05_sum_2_arr_MPI+threads+cuda/main.cpp
+++ labs/lab05_sum_2_arr_MPI+threads+cuda/main.cpp
@@ -1,14 +1,9 @@
 #include <mpi.h>
 #include <iostream>
-
 #include "utils.h"
-//#include "cpuThreads.h"
-//#include "gpu.h"
-
 #include "sum.h"
 
 using namespace std;
-
 
 int main (int argc, char* argv[])
 {    
@@ -31,16 +26,7 @@ int main (int argc, char* argv[])
     double t2 = MPI_Wtime();  
     double t = t2-t1;
     printf("Rank %d: Time of testThreads: %lf sec\n",rank, t);
-
-    //printGpuParameters("Node " + std::to_string(rank));
-
-    //int res_gpu = gpu(5, 15);
-    //std::cerr<<"res_gpu = "<<res_gpu<<" (rank = "<<rank<<")"<<std::endl;
-
-    //multiGpuTest();
-
-    //multiGpuTest2();
-
+    
     MPI_Finalize();
     return 0;
 }
