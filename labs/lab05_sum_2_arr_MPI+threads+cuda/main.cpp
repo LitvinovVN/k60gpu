c01d9230227e366df5df9ef29d25bca94f41d84f
LitvinovVN <LitvinovVN@rambler.ru>
2021-07-13 11:42:13 +0300

    13.07.2021_11:42:13,78

--- labs/lab05_sum_2_arr_MPI+threads+cuda/main.cpp
+++ labs/lab05_sum_2_arr_MPI+threads+cuda/main.cpp
@@ -59,6 +59,10 @@ void testSum2Arrays(int mpi_rank, int mpi_size,
     {        
         std::cerr << "a[" << i << "] = " << a[i] << "; b[" << i << "] = " << b[i] << "; c[" << i << "] = " << c[i] << "; c_par[" << i << "] = " << c_par[i] << std::endl;
     }
+
+    // Вывод неверно рассчитанных элементов массивов
+
+
 }
 
 int main (int argc, char* argv[])
@@ -68,7 +72,8 @@ int main (int argc, char* argv[])
     MPI_Barrier( MPI_COMM_WORLD );
 
     double t1 = MPI_Wtime();
-    testSum2Arrays(rank, size, 1, 62 * 10000000, 0, 20000000);
+    //testSum2Arrays(rank, size, 1, 62 * 10000000, 0, 20000000);
+    testSum2Arrays(rank, size, 2, 31 * 10000000, 0, 20000000);
     //testSum2Arrays(rank, size, 62, 10000000, 4, 20000000);
     double t2 = MPI_Wtime();  
     double t = t2-t1;
