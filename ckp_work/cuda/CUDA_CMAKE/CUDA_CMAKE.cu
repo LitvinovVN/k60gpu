f35af54a9f00c540862b04c4a14645b2df02a224
litvinovvn <litvinovvn@rambler.ru>
2021-10-29 09:20:10 +0300

    29.10.2021_ 9:20:09.92

---
+++ ckp_work/cuda/CUDA_CMAKE/CUDA_CMAKE.cu
@@ -0,0 +1,13 @@
+﻿// CUDA_CMAKE.cpp: определяет точку входа для приложения.
+//
+
+#include "CUDA_CMAKE.h"
+#include "cuda.h"
+using namespace std;
+
+int main()
+{
+	cudaError_t cudaStatus;
+	cout << "Hello CMake." << endl;
+	return 0;
+}
---
+++ ckp_work/cuda/CUDA_CMAKE/build.bat
@@ -0,0 +1,7 @@
+mkdir _build
+cd _build
+cmake ..
+cmake --build .
+
+cd Debug
+CUDA_CMAKE.exe
